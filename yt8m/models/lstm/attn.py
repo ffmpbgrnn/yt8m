d80bc9b85a5c1640360e006afa3c7bb4f97d371b
Linchao Zhu <zhulinchao7@gmail.com>
2017-02-25 12:07:20 +1100

    update

---
+++ yt8m/models/lstm/attn.py
@@ -0,0 +1,111 @@
+from tensorflow.python.ops import variable_scope
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import nn_ops
+from tensorflow.contrib.rnn.python.ops import core_rnn_cell_impl
+from tensorflow.python.util import nest
+from tensorflow.python.ops import math_ops
+
+linear = core_rnn_cell_impl._linear  # pylint: disable=protected-access
+
+def attention_decoder(decoder_inputs,
+                      initial_state,
+                      attention_states,
+                      cell,
+                      output_size=None,
+                      num_heads=1,
+                      dtype=None,
+                      scope=None,
+                      initial_state_attention=False):
+  if not decoder_inputs:
+    raise ValueError("Must provide at least 1 input to attention decoder.")
+  if num_heads < 1:
+    raise ValueError("With less than 1 heads, use a non-attention decoder.")
+  if attention_states.get_shape()[2].value is None:
+    raise ValueError("Shape[2] of attention_states must be known: %s" %
+                     attention_states.get_shape())
+  if output_size is None:
+    output_size = cell.output_size
+
+  with variable_scope.variable_scope(
+      scope or "attention_decoder", dtype=dtype) as scope:
+    dtype = scope.dtype
+
+    batch_size = array_ops.shape(decoder_inputs[0])[0]  # Needed for reshaping.
+    attn_length = attention_states.get_shape()[1].value
+    if attn_length is None:
+      attn_length = array_ops.shape(attention_states)[1]
+    attn_size = attention_states.get_shape()[2].value
+
+    # To calculate W1 * h_t we use a 1-by-1 convolution, need to reshape before.
+    hidden = array_ops.reshape(attention_states,
+                               [-1, attn_length, 1, attn_size])
+    hidden_features = []
+    v = []
+    # TODO
+    attention_vec_size = attn_size  # Size of query vectors for attention.
+    for a in xrange(num_heads):
+      k = variable_scope.get_variable("AttnW_%d" % a,
+                                      [1, 1, attn_size, attention_vec_size])
+      hidden_features.append(nn_ops.conv2d(hidden, k, [1, 1, 1, 1], "SAME"))
+      v.append(
+          variable_scope.get_variable("AttnV_%d" % a, [attention_vec_size]))
+
+    state = initial_state
+
+    def attention(query):
+      """Put attention masks on hidden using hidden_features and query."""
+      ds = []  # Results of attention reads will be stored here.
+      if nest.is_sequence(query):  # If the query is a tuple, flatten it.
+        query_list = nest.flatten(query)
+        for q in query_list:  # Check that ndims == 2 if specified.
+          ndims = q.get_shape().ndims
+          if ndims:
+            assert ndims == 2
+        query = array_ops.concat(query_list, 1)
+      for a in xrange(num_heads):
+        with variable_scope.variable_scope("Attention_%d" % a):
+          y = linear(query, attention_vec_size, True)
+          y = array_ops.reshape(y, [-1, 1, 1, attention_vec_size])
+          # Attention mask is a softmax of v^T * tanh(...).
+          s = math_ops.reduce_sum(v[a] * math_ops.tanh(hidden_features[a] + y),
+                                  [2, 3])
+          a = nn_ops.softmax(s)
+          # Now calculate the attention-weighted vector d.
+          d = math_ops.reduce_sum(
+              array_ops.reshape(a, [-1, attn_length, 1, 1]) * hidden, [1, 2])
+          ds.append(array_ops.reshape(d, [-1, attn_size]))
+      return ds
+
+    outputs = []
+    batch_attn_size = array_ops.stack([batch_size, attn_size])
+    attns = [
+        array_ops.zeros(
+            batch_attn_size, dtype=dtype) for _ in xrange(num_heads)
+    ]
+    for a in attns:  # Ensure the second shape of attention vectors is set.
+      a.set_shape([None, attn_size])
+    if initial_state_attention:
+      attns = attention(initial_state)
+    for i, inp in enumerate(decoder_inputs):
+      if i > 0:
+        variable_scope.get_variable_scope().reuse_variables()
+      # Merge input and previous attentions into one vector of the right size.
+      input_size = inp.get_shape().with_rank(2)[1]
+      if input_size.value is None:
+        raise ValueError("Could not infer input size from input: %s" % inp.name)
+      x = linear([inp] + attns, input_size, True)
+      # Run the RNN.
+      cell_output, state = cell(x, state)
+      # Run the attention mechanism.
+      if i == 0 and initial_state_attention:
+        with variable_scope.variable_scope(
+            variable_scope.get_variable_scope(), reuse=True):
+          attns = attention(state)
+      else:
+        attns = attention(state)
+
+      with variable_scope.variable_scope("AttnOutputProjection"):
+        output = linear([cell_output] + attns, output_size, True)
+      outputs.append(output)
+
+  return outputs, state
---
+++ yt8m/models/lstm/lstm_enc_dec.py
@@ -0,0 +1,77 @@
+import math
+
+import tensorflow as tf
+
+import tensorflow.contrib.slim as slim
+
+from tensorflow.contrib.rnn.python.ops import core_rnn_cell
+from tensorflow.contrib.legacy_seq2seq.python.ops import seq2seq as seq2seq_lib
+
+from yt8m.models import models
+import yt8m.models.model_utils as utils
+import attn
+
+class LSTMEncoder(models.BaseModel):
+  def __init__(self):
+    super(LSTMEncoder, self).__init__()
+
+    self.normalize_input = False
+    self.clip_global_norm = 5
+    self.var_moving_average_decay = 0.9997
+    self.optimizer_name = "AdamOptimizer"
+    self.base_learning_rate = 3e-4
+
+    self.cell_size = 1024
+    self.max_steps = 30
+
+  def create_model(self, model_input, vocab_size, num_frames,
+                   is_training=True, labels=None, **unused_params):
+
+    self.phase_train = is_training
+    num_frames = tf.cast(tf.expand_dims(num_frames, 1), tf.float32)
+    model_inputs = utils.SampleRandomSequence(model_input, num_frames,
+                                              self.max_steps)
+
+    enc_cell = self.get_enc_cell(self.cell_size, vocab_size)
+    dec_cell = self.get_dec_cell(self.cell_size)
+    runtime_batch_size = tf.shape(model_inputs)[0]
+
+    with tf.variable_scope("Enc"):
+      enc_init_state = enc_cell.zero_state(runtime_batch_size, dtype=tf.float32)
+      enc_outputs, enc_state = tf.nn.dynamic_rnn(
+          enc_cell, model_inputs, initial_state=enc_init_state, scope="enc")
+
+    decoder_inputs = ""
+    dec_targets = ""
+    dec_weights = ""
+    # enc_outputs_lists = tf.split(enc_outputs, num_or_size_splits=self.max_steps, axis=1)
+    dec_outputs, _ = attn.attention_decoder(
+        decoder_inputs, initial_state=enc_state,
+        attention_states=enc_outputs,
+        cell=dec_cell, output_size=vocab_size,
+        dtype=tf.float32)
+    loss = seq2seq_lib.sequence_loss(
+        dec_outputs, dec_targets, dec_weights,
+        softmax_loss_function=None)
+    logits = tf.reduce_mean(dec_outputs, axis=1)
+    # logits = tf.nn.sigmoid(enc_outputs[:, -1, :])
+    return {
+        "predictions": logits,
+        "loss": loss,
+    }
+
+  def get_enc_cell(self, cell_size, vocab_size):
+    cell = core_rnn_cell.GRUCell(cell_size)
+    if self.phase_train:
+      cell = core_rnn_cell.DropoutWrapper(
+          cell, input_keep_prob=0.5, output_keep_prob=0.5)
+    cell = core_rnn_cell.InputProjectionWrapper(cell, cell_size)
+    cell = core_rnn_cell.OutputProjectionWrapper(cell, cell_size)
+    return cell
+
+  def get_dec_cell(self, cell_size):
+    cell = core_rnn_cell.GRUCell(cell_size)
+    if self.phase_train:
+      cell = core_rnn_cell.DropoutWrapper(
+          cell, input_keep_prob=0.5, output_keep_prob=0.5)
+    return cell
