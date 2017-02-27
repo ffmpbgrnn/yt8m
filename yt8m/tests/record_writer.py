f3e4084fb84b90b34b41a900b3da91faabf851fa
Linchao Zhu <zhulinchao7@gmail.com>
2017-02-27 20:44:09 +1100

    update

--- yt8m/tests/record_writer.py
+++ yt8m/tests/record_writer.py
@@ -1,5 +1,7 @@
 import numpy as np
+import cPickle as pkl
 
+import h5py
 import tensorflow as tf
 from tensorflow import gfile
 
@@ -37,56 +39,75 @@ def matrix_to_tfexample(data, labels, video_id):
   }))
 
 
-output_filename = "/tmp/a.tfrecord"
-'''
-d = np.array(np.random.rand(256, 256), dtype=np.float32)
-print(d)
-with tf.python_io.TFRecordWriter(output_filename) as tfrecord_writer:
-  example = matrix_to_tfexample(
-      d.tostring(), [1, 2], video_id="asf")
-  tfrecord_writer.write(example.SerializeToString())
+def write():
+  output_filename = "/tmp/a.tfrecord"
+  d = np.array(np.random.rand(256, 256), dtype=np.float16)
+  print(d)
+  with tf.python_io.TFRecordWriter(output_filename) as tfrecord_writer:
+    example = matrix_to_tfexample(
+        d.tostring(), [1, 2], video_id="asf")
+    tfrecord_writer.write(example.SerializeToString())
 
-'''
+def write_from_hdfs():
+  train_vid_to_labels = pkl.load(open("/data/D2DCRC/linchao/YT/train_vid_to_labels.pkl"))
+  split_id = ""
+  fin = h5py.File("/data/D2DCRC/linchao/YT/vlad_feas_256_256_nonorm/train/feas_{0}.h5".format(split_id))
+  vids = fin.keys()
+  writer_counter = 0
+  for vid in vids:
+    data = fin[vid].value
+    data = np.array(data, dtype=np.float16)
+    labels = [int(_) for _ in train_vid_to_labels[vid]]
+    example = matrix_to_tfexample(
+        data.tostring(), labels, video_id=vid)
+    if writer_counter % 1200 == 0:
+      shard_id = int(writer_counter / 1200)
+      tfrecord_writer = tf.python_io.TFRecordWriter("/data/D2DCRC/linchao/YT/vlad_feas_256_256_nonorm/train_tfrecord/train_{0}_{1}.tfrecord".format(split_id, shard_id))
+    tfrecord_writer.write(example.SerializeToString())
+    writer_counter += 1
+
+
+def read(output_filename):
+  files = gfile.Glob(output_filename)
+  filename_queue = tf.train.string_input_producer(
+        files, shuffle=False, num_epochs=1)
+
+  reader = tf.TFRecordReader()
+  _, serialized_example = reader.read(filename_queue)
+
+  keys_to_features={
+      'video_id':
+          tf.FixedLenFeature(
+              (), tf.string, default_value=''),
+      'labels':
+          tf.VarLenFeature(tf.int64),
+      'feas':
+          tf.FixedLenFeature(
+              (), tf.string, default_value=''),
+  }
+  contexts = tf.parse_single_example(
+      serialized_example, keys_to_features)
+  feas = contexts["feas"]
+  feas = tf.reshape(tf.decode_raw(feas, tf.float32), [-1])
+
+  dense_labels = (tf.cast(
+          tf.sparse_to_dense(contexts["labels"].values, (10,), 1,
+              validate_indices=False),
+          tf.bool))
+
+  with tf.Session() as sess:
+    sess.run(tf.local_variables_initializer())
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+    # feas_val, labels, video_id = sess.run([feas, contexts["labels"], contexts["video_id"]])
+    feas_val, labels, video_id = sess.run([feas, dense_labels, contexts["video_id"]])
+    print(feas_val.shape)
+    print(np.reshape(feas_val, [256, 256]))
+    print(labels)
+    print(video_id)
+    # print(sess.run(labels))
+    coord.request_stop()
+    coord.join(threads)
+
 # for example in tf.python_io.tf_record_iterator(output_filename):
   # d = tf.train.Example.FromString(example)
-
-files = gfile.Glob(output_filename)
-filename_queue = tf.train.string_input_producer(
-      files, shuffle=False, num_epochs=1)
-
-reader = tf.TFRecordReader()
-_, serialized_example = reader.read(filename_queue)
-
-keys_to_features={
-    'video_id':
-        tf.FixedLenFeature(
-            (), tf.string, default_value=''),
-    'labels':
-        tf.VarLenFeature(tf.int64),
-    'feas':
-        tf.FixedLenFeature(
-            (), tf.string, default_value=''),
-}
-contexts = tf.parse_single_example(
-    serialized_example, keys_to_features)
-feas = contexts["feas"]
-feas = tf.reshape(tf.decode_raw(feas, tf.float32), [-1])
-
-dense_labels = (tf.cast(
-        tf.sparse_to_dense(contexts["labels"].values, (10,), 1,
-            validate_indices=False),
-        tf.bool))
-
-with tf.Session() as sess:
-  sess.run(tf.local_variables_initializer())
-  coord = tf.train.Coordinator()
-  threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-  # feas_val, labels, video_id = sess.run([feas, contexts["labels"], contexts["video_id"]])
-  feas_val, labels, video_id = sess.run([feas, dense_labels, contexts["video_id"]])
-  print(feas_val.shape)
-  print(np.reshape(feas_val, [256, 256]))
-  print(labels)
-  print(video_id)
-  # print(sess.run(labels))
-  coord.request_stop()
-  coord.join(threads)
