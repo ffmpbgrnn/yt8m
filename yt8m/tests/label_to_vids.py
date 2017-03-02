f800c36270dfc1c8285ef2eb428ec2946192a93b
Linchao Zhu <zhulinchao7@gmail.com>
2017-03-02 23:30:14 +0000

    update

--- yt8m/tests/label_to_vids.py
+++ yt8m/tests/label_to_vids.py
@@ -1,7 +1,8 @@
-import pkl
+import cPickle as pkl
 
 
-vid_to_labels = pkl.load(open("/data/uts700/linchao/yt8m/YT/data/vid_info/train_vid_to_labels.pkl"))
+# vid_to_labels = pkl.load(open("/data/uts700/linchao/yt8m/YT/data/vid_info/train_vid_to_labels.pkl"))
+
 label_to_vids = {}
 cnt = 0
 for vid, labels in vid_to_labels.iteritems():
@@ -10,4 +11,4 @@ for vid, labels in vid_to_labels.iteritems():
         c.append(vid)
         label_to_vids[l] = c
         cnt += 1
-# pkl.dump(label_to_vids, open("/data/uts700/linchao/yt8m/YT/data/vid_info/train_label_to_vids.pkl", "w"))
+pkl.dump(label_to_vids, open("/data/uts700/linchao/yt8m/YT/data/vid_info/train_label_to_vids.pkl", "w"))
