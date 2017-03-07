40d566e288b29388b8b957e7138f125f19a9a683
Linchao Zhu <zhulinchao7@gmail.com>
2017-03-07 14:05:39 +1100

    update

--- yt8m/models/lstm/lstm_memnet.py
+++ yt8m/models/lstm/lstm_memnet.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from tensorflow.python.ops import variable_scope
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import variable_scope as vs
 from tensorflow.python.ops import nn_ops
 from tensorflow.python.ops import embedding_ops
 from tensorflow.python.ops import math_ops
@@ -18,7 +19,61 @@ from tensorflow.contrib.legacy_seq2seq.python.ops import seq2seq as seq2seq_lib
 from yt8m.models import models
 import yt8m.models.model_utils as utils
 
-linear = core_rnn_cell_impl._linear  # pylint: disable=protected-access
+# linear = core_rnn_cell_impl._linear  # pylint: disable=protected-access
+
+def linear(args, output_size, bias, bias_start=0.0, reuse=False):
+  """Linear map: sum_i(args[i] * W[i]), where W[i] is a variable.
+
+  Args:
+    args: a 2D Tensor or a list of 2D, batch x n, Tensors.
+    output_size: int, second dimension of W[i].
+    bias: boolean, whether to add a bias term or not.
+    bias_start: starting value to initialize the bias; 0 by default.
+
+  Returns:
+    A 2D Tensor with shape [batch x output_size] equal to
+    sum_i(args[i] * W[i]), where W[i]s are newly created matrices.
+
+  Raises:
+    ValueError: if some of the arguments has unspecified or wrong shape.
+  """
+  if args is None or (nest.is_sequence(args) and not args):
+    raise ValueError("`args` must be specified")
+  if not nest.is_sequence(args):
+    args = [args]
+
+  # Calculate the total size of arguments on dimension 1.
+  total_arg_size = 0
+  shapes = [a.get_shape() for a in args]
+  for shape in shapes:
+    if shape.ndims != 2:
+      raise ValueError("linear is expecting 2D arguments: %s" % shapes)
+    if shape[1].value is None:
+      raise ValueError("linear expects shape[1] to be provided for shape %s, "
+                       "but saw %s" % (shape, shape[1]))
+    else:
+      total_arg_size += shape[1].value
+
+  dtype = [a.dtype for a in args][0]
+
+  # Now the computation.
+  scope = vs.get_variable_scope()
+  with vs.variable_scope(scope, reuse=reuse) as outer_scope:
+    weights = vs.get_variable(
+        "weights", [total_arg_size, output_size], dtype=dtype)
+    if len(args) == 1:
+      res = math_ops.matmul(args[0], weights)
+    else:
+      res = math_ops.matmul(array_ops.concat(args, 1), weights)
+    if not bias:
+      return res
+    with vs.variable_scope(outer_scope, reuse=reuse) as inner_scope:
+      inner_scope.set_partitioner(None)
+      biases = vs.get_variable(
+          "biases", [output_size],
+          dtype=dtype,
+          initializer=init_ops.constant_initializer(bias_start, dtype=dtype))
+    return nn_ops.bias_add(res, biases)
 
 def _extract_argmax_and_embed(embedding,
                               output_projection=None,
@@ -280,8 +335,9 @@ class LSTMMemNet(models.BaseModel):
                 if not first:
                   print(idx)
                   tf.get_variable_scope().reuse_variables()
+                output = linear([split], vocab_size, True, reuse=not first)
+                if not first:
                   first = False
-                output = linear([split], vocab_size, True)
                 pred = tf.nn.softmax(output)
                 pred = pred * tf.tile(nf[:, 0:1], [1, vocab_size])
                 preds.append(pred)
